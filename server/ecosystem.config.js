874d923f838d4b0e9473c0ec4e1c2877fad8e8db
sumstudent <deo.sumatra@yahoo.com>
2022-11-20 21:15:19 +0800

    Modified PM2 config file

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -4,10 +4,10 @@ module.exports = {
     script: "./src/server.js",
     instances: "max",
     exec_mode: "cluster",
-    out_file: "./src.logs/out.log",
+    out_file: "./src/logs/out.log",
     error_file: "./src/logs/errors.log",
     merge_logs: true,
-    env: {
+    env_development: {
       NODE_ENV: "development",
       NODE_CLUSTER_SCHED_POLICY: "rr"
     },
