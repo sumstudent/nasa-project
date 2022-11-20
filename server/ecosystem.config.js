1e29f9b5783c28345c33f3a1362ed744c5b27d1a
sumstudent <deo.sumatra@yahoo.com>
2022-11-20 14:49:30 +0800

    Added config file for PM2, with round robin scheduling.

---
+++ server/ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [{
+    name: "server",
+    script: "./src/server.js",
+    instances: "max",
+    exec_mode: "cluster",
+    out_file: "./src.logs/out.log",
+    error_file: "./src/logs/errors.log",
+    merge_logs: true,
+    env: {
+      NODE_ENV: "development",
+      NODE_CLUSTER_SCHED_POLICY: "rr"
+    },
+    env_production: {
+      NODE_ENV: "production"
+    }
+  }]
+}
