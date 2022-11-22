d1882e917c6d3882d44c53fc522becd0a9ee2c93
Deo Sumatra <61895002+sumstudent@users.noreply.github.com>
2022-11-22 21:36:42 +0800

    Database integration (#2)
    * Added mongoose package for MongoDb functionalities
    
    * Modified server.js for MongoDb connection
    
    * Removed the unnecessary objects param
    
    * Added dotenv package into devdepndcs
    
    * Modified server.js to hide sensitive variables.

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,26 @@
 const http = require('http');
+const mongoose = require('mongoose');
 const app = require('./app');
+const path = require('path')
+
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env.local') });
 
 const { loadPlanetsData } = require('./models/planets.model')
 
 const PORT = process.env.PORT || 8000;
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+})
+
 async function startServer() {
+    await mongoose.connect(process.env.DB_URL);
     await loadPlanetsData();
 
     server.listen(PORT, () => {
