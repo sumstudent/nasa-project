4fc001c68ac5db9702e04d20b95fb186fbec52b4
Deo Sumatra <61895002+sumstudent@users.noreply.github.com>
2022-11-26 02:16:24 +0800

    Database integration (#5)
    * Removed build files
    
    * Modified CSV File
    
    * Initialized planet collection schema
    
    * TODO: planets.model
    
    * Initialized planet collection scheme
    
    * Removed assets
    
    * Added assets in the public folder
    
    * Adding files from the public folder
    
    * Modified to no longer ignore files from public
    
    * Pushing build file to delete later
    
    * Modified ignore file
    
    * Removing build files
    
    * Updated ignore file
    
    * Removed build files from the client folder
    
    * Adding the assets from the public folder
    
    * Modified a column name in the CSV file
    
    * Modified planet model; can now update and insert

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,11 +2,18 @@ const { parse } = require('csv-parse');
 const path = require('path')
 const fs = require('fs');
 
-const habitablePlanets = [];
+const planets = require('./planets.mongo');
+/**
+* data.updateOne(
+* {filter; if data dont exists, update this block}, 
+* {second arg; if data does exists, update this block},
+* {upsert: true; })
+*/
 
 function isHabitablePlanet(planet) {
     return planet['koi_disposition'] === 'CONFIRMED'
-        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_insol'] > 0.36
+        && planet['koi_insol'] < 1.11
         && planet['koi_prad'] < 1.6;
 }
 
@@ -17,23 +24,36 @@ function loadPlanetsData() {
                 comment: '#',
                 columns: true,
             }))
-            .on('data', (data) => {
+            .on('data', async (data) => {
                 if (isHabitablePlanet(data)) {
-                    habitablePlanets.push(data);
+                    savePlanet(data);
                 }
             })
             .on('error', (err) => {
                 reject(err);
             })
-            .on('end', () => {
-                console.log(`${habitablePlanets.length} habitable planets found!`);
+            .on('end', async () => {
                 resolve();
             });
     });
 }
 
-function getAllPlanets() {
-    return habitablePlanets;
+async function getAllPlanets() {
+    return await planets.find({});
+}
+
+async function savePlanet(planet) {
+    try {
+        await planets.updateOne({
+            keplerName: planet.kepler_name
+        }, {
+            keplerName: planet.kepler_name,
+        }, {
+            upsert: true,
+        });
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 module.exports = {
