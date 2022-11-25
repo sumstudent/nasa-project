2890892dea9adfec9bbc000c8c2e11ff9f2bd360
sumstudent <deo.sumatra@yahoo.com>
2022-11-26 02:12:25 +0800

    Modified planet model; can now update and insert

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,13 +2,18 @@ const { parse } = require('csv-parse');
 const path = require('path')
 const fs = require('fs');
 
-const habitablePlanets = [];
 const planets = require('./planets.mongo');
-
+/**
+* data.updateOne(
+* {filter; if data dont exists, update this block}, 
+* {second arg; if data does exists, update this block},
+* {upsert: true; })
+*/
 
 function isHabitablePlanet(planet) {
     return planet['koi_disposition'] === 'CONFIRMED'
-        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_insol'] > 0.36
+        && planet['koi_insol'] < 1.11
         && planet['koi_prad'] < 1.6;
 }
 
@@ -21,25 +26,34 @@ function loadPlanetsData() {
             }))
             .on('data', async (data) => {
                 if (isHabitablePlanet(data)) {
-                    //TODO: Replace below create with Upsert; Update and Insert
-                    // habitablePlanets.push(data);
-                    // await planets.create({
-                    //     keplerName: data.kepler_Name
-                    // });
+                    savePlanet(data);
                 }
             })
             .on('error', (err) => {
                 reject(err);
             })
-            .on('end', () => {
-                console.log(`${habitablePlanets.length} habitable planets found!`);
+            .on('end', async () => {
                 resolve();
             });
     });
 }
 
-function getAllPlanets() {
-    return habitablePlanets;
+async function getAllPlanets() {
+    return await planets.find({});
+}
+
+async function savePlanet(planet) {
+    try {
+        await planets.updateOne({
+            keplerName: planet.kepler_name
+        }, {
+            keplerName: planet.kepler_name,
+        }, {
+            upsert: true,
+        });
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 module.exports = {
